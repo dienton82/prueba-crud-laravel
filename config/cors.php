0051625eb8ebbedb60066077e574465d18955f7c
Eduardo Alvarez <yalvarezg82@gmail.com>
2025-07-09 20:13:49 -0500

    fix: allow frontend domain in CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+    'http://localhost:5173', // tu entorno local de Vite
+    'https://crud-frontend-gwxm.onrender.com', // tu site en Render
+],
 
     'allowed_origins_patterns' => [],
 
